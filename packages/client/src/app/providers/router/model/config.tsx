426a37f4ae772ccbb23277e715bcb150817c7466
ELitvinova <e.litvinova@elfsight.com>
2024-08-29 17:45:50 +0200

    error page

--- packages/client/src/app/providers/router/model/config.tsx
+++ packages/client/src/app/providers/router/model/config.tsx
@@ -3,12 +3,11 @@ import { RouteNames, routePaths } from '@/shared/constants/router'
 import { MainLayout } from '@/app/layouts/main/MainLayout'
 import { MainPage } from '@/pages/main'
 import { ProfilePage } from '@/pages/profile'
-import { ForbiddenPage } from '@/pages/forbidden'
-import { NotFoundPage } from '@/pages/not-found'
 import { StartGamePage } from '@/pages/start-game'
 import { EndGamePage } from '@/pages/end-game'
 import { Authorization, Registration } from '@/pages/auth'
 import { Leaderboard } from '@/pages/leaderboard'
+import { ErrorPage } from '@/pages/error'
 
 export const router = createBrowserRouter([
   {
@@ -28,7 +27,7 @@ export const router = createBrowserRouter([
       },
       {
         path: routePaths[RouteNames.FORBIDDEN],
-        element: <ForbiddenPage />,
+        element: <ErrorPage text={'500'} />,
       },
       {
         path: routePaths[RouteNames.START_GAME],
@@ -48,7 +47,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '*',
-        element: <NotFoundPage />,
+        element: <ErrorPage text={'404'} />,
       },
     ],
   },
