78c86d82f0ff7714b0b496ec9f4fe8def2582759
Дмитрий Пропастин <dima.propastin@gmail.com>
2024-08-31 22:14:02 +0300

    feat: Add forum pages
    Closes: MEM-17

--- packages/client/src/shared/config/dayjs/index.ts
+++ packages/client/src/shared/config/dayjs/index.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/ru'
 
-dayjs.extend(relativeTime)
-dayjs.locale('ru')
+dayjs?.extend(relativeTime)
+dayjs?.locale('ru')
 
 export default dayjs
