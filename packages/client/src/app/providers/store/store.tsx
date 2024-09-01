000fe9baebcacd056c59de0e3be8bb6893866de4
Pavel <zabolotnikovpg2@mail.ru>
2024-09-01 12:44:27 +0300

    fix: вынес store в app/providers/store

--- packages/client/src/app/providers/store/store.tsx
+++ packages/client/src/app/providers/store/store.tsx
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { rootReducer } from './reducers'
+import { rootReducer } from '../../../shared/services/reducers'
 
 export const store = configureStore({
   reducer: rootReducer,
