73d987821a832f7346bfa3bdf9254c3baf4efeda
Pavel <zabolotnikovpg2@mail.ru>
2024-09-01 12:40:33 +0300

    fix: вынес services в слой shared

--- packages/client/src/shared/lib/store/redux.ts
+++ packages/client/src/shared/lib/store/redux.ts
@@ -1,4 +1,4 @@
-import { AppDispatch, RootState } from '@/services/types'
+import { AppDispatch, RootState } from '@/shared/services/types'
 import { useDispatch, useSelector } from 'react-redux'
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
