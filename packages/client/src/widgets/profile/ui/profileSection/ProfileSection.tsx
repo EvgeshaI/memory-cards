b4c179ea7cb7b8731b075bd875845067966dc222
Uliana Agarova <uagarova@gmail.com>
2024-09-06 17:53:37 +0100

    вынесла логику профиля в widget
    переместила бизнес-логику проифля из pages в widget

---
+++ packages/client/src/widgets/profile/ui/profileSection/ProfileSection.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { PasswordModal, ProfileInfo } from '@/widgets/profile';
+import { useUserData, useLogout } from '@/entities/user';
+import { handleAvatarChange } from '@/features/profile';
+import { Notification, Loader } from '@/shared/ui';
+import cls from './Profile.module.scss';
+import { RESOURCES_URL } from '@/shared/constants/api';
+
+export const ProfileSection: React.FC = () => {
+  const { user, isLoading } = useUserData();
+  const { handleLogout } = useLogout();
+  const [avatar, setAvatarUrl] = useState(user?.avatar || '');
+  const [isPasswordModalOpen, setPasswordModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (user?.avatar) {
+      setAvatarUrl(user.avatar);
+    }
+  }, [user]);
+
+  if (isLoading) return <Loader size="xl" variant="bars" />;
+  if (!user)
+    return (
+      <Notification color="red" title="Ошибка">
+        Пользователь не существует
+      </Notification>
+    );
+
+  const avatarUrl = avatar ? `${RESOURCES_URL}${avatar}` : '';
+
+  return (
+    <div className={cls.root}>
+      <ProfileInfo
+        userId={user.id}
+        avatarUrl={avatarUrl}
+        firstName={user.first_name}
+        secondName={user.second_name}
+        email={user.email}
+        onAvatarChange={(newAvatarUrl) =>
+          handleAvatarChange(newAvatarUrl, setAvatarUrl)
+        }
+        onPasswordChange={() => setPasswordModalOpen(true)}
+        onLogout={handleLogout}
+      />
+      <PasswordModal
+        isOpen={isPasswordModalOpen}
+        onClose={() => setPasswordModalOpen(false)}
+        userId={user.id}
+      />
+    </div>
+  );
+};
