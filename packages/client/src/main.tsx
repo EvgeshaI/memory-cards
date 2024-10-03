a56948a7aaf6a755030a25b663201aa88c54aeb9
Pavel <zabolotnikovpg2@mail.ru>
2024-10-03 23:44:07 +0300

    fix: выполнение задания согласно теории

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,8 +1,10 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { App } from './app';
 
-const container = document.getElementById('root') as HTMLElement;
-
-const root = createRoot(container);
-
-root.render(<App />);
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
